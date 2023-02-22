7b9168dbada74a584f5a122b30c5b22f733e42bb
Steven Hertz <hertzsteven@icloud.com>
2023-02-22 09:33:53 -0500

    Initial Commit

---
+++ GetUserDataFromExhibit/GetUserDataFromExhibitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetUserDataFromExhibitApp.swift
+//  GetUserDataFromExhibit
+//
+//  Created by Steven Hertz on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct GetUserDataFromExhibitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
